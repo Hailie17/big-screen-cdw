658c7d12128b8d92a536c4f92acabab16e302c07
Hailie17 <192151452@qq.com>
2023-11-10 19:26:21 +0800

    大屏适配方案

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
 import { RouterView } from "vue-router";
+import useScalePage from "@/hooks/useScalePage";
+
+let option = {
+  targetX: 1920,
+  targetY: 1080,
+  tragetRatio: 16 / 9,
+};
+
+useScalePage(option);
 </script>
 
 <template>
