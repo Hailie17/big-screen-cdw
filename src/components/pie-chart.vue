dd6c52339b888a8c9b8f5f62acfeeb86e9e87c01
Hailie17 <192151452@qq.com>
2023-11-13 01:54:16 +0800

    网络请求

--- src/components/pie-chart.vue
+++ src/components/pie-chart.vue
@@ -7,6 +7,9 @@
 import * as echarts from "echarts";
 import { onMounted, ref, watch } from "vue";
 import useEcharts from "@/hooks/useEcharts";
+import { getPowerScreenData } from "@/services";
+
+getPowerScreenData().then((res) => console.log(res));
 
 const props = defineProps({
   width: {
@@ -86,45 +89,6 @@ function setupEchart(echartDatas) {
 }
 
 function getOption(pieDatas = []) {
-  // let pieDatas = [
-  // {
-  //   value: 100,
-  //   name: "广州占比",
-  //   percentage: "5%",
-  //   color: "#34D160",
-  // },
-  // {
-  //   value: 200,
-  //   name: "深圳占比",
-  //   percentage: "4%",
-  //   color: "#027FF2",
-  // },
-  // {
-  //   value: 300,
-  //   name: "东莞占比",
-  //   percentage: "8%",
-  //   color: "#8A00E1",
-  // },
-  // {
-  //   value: 400,
-  //   name: "佛山占比",
-  //   percentage: "10%",
-  //   color: "#F19610",
-  // },
-  // {
-  //   value: 500,
-  //   name: "中山占比",
-  //   percentage: "20%",
-  //   color: "#6054FF",
-  // },
-  // {
-  //   value: 600,
-  //   name: "珠海占比",
-  //   percentage: "40%",
-  //   color: "#00C6FF",
-  // },
-  // ];
-
   let colors = pieDatas.map((item) => {
     return item.color;
   });
--- src/services/request/index.js
+++ src/services/request/index.js
@@ -1,28 +1,32 @@
-import axios from 'axios'
-import { BASE_URL, TIMEOUT } from './config'
+import axios from "axios"; // 导入axios
+import { BASE_URL, TIMEOUT } from "./config";
+// 创建类
 class HYRequest {
-  constructor(baseURL = BASE_URL, timeout= TIMEOUT) {
+  constructor(baseURL = BASE_URL, timeout = TIMEOUT) {
+    // 创建axios实例
     this.instance = axios.create({
       baseURL,
-      timeout
-    })
+      timeout,
+    });
   }
+  // 定义了通用request请求。将返回结果改成promise
   request(config) {
     return new Promise((resolve, reject) => {
-      this.instance.request(config).then(res => {
-        resolve(res.data)
-      }).catch(err => {
-        reject(err)
-      })
-    })
+      this.instance
+        .request(config)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
   }
   get(config) {
-    return this.request({ ...config, method: "get" })
+    return this.request({ ...config, method: "get" });
   }
   post(config) {
-    return this.request({ ...config, method: "post" })
+    return this.request({ ...config, method: "post" });
   }
 }
-export default new HYRequest()
-
-
+export default new HYRequest(); // 导出构造器方法
